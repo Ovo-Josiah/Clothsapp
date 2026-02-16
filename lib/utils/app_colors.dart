bb336bbb7944729ece2ed054dafd837b07e8e356
ovo-josiah <alojosiah6@gmail.com>
2026-02-16 15:28:08 +0100

    App colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFB30226);
+  static const Color secondaryColor = Color(0xFFB30226);
+  static const Color backgroundColor = Color(0xFFF9F9F9);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color tertiaryColor = Color(0xFF222222);
+  static const Color starColor = Color(0xFFFFBA49);
+  static const Color starBoarder = Color(0xFF9B9B9B);
+}
---
+++ lib/utils/button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:clothesapp/utils/app_colors.dart';
+
+class Button extends StatelessWidget {
+  const Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.secondaryColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'Button',
+            style: const TextStyle(
+              fontSize: 16,
+              fontFamily: "Metropolis",
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
